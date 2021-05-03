1e398bd6e56098c3a2b8ace74a848e4cab5490de
Kang Hyojun <ed@hopsoffice.com>
2021-05-03 23:08:01 +0900

    근무시간 캘린더 연동

--- README.md
+++ README.md
@@ -32,6 +32,34 @@ $ cat resource.json
 }
 ```
 
+구글 연동을 위해 아래의 값들이 필요합니다.
+
+```
+{
+    ...
+    "google": {
+      "client_id": "",
+      "client_secret": "",
+      "project_id": "",
+      "scopes": [
+            "https://www.googleapis.com/auth/calendar",
+      ],
+      "work_calendar_id": ""
+    },
+    "token_bucket_name": "",
+    ..
+}
+```
+
+[Google Cloud API][gcp-api]에서 프로젝트를 만들고 OAuth Application을 만들어서
+`client_id`, `client_secret`, `project_id`를 설정해주세요. 인증 토큰을
+저장하기 위해 s3를 사용합니다. s3에 버켓을 만들고 `token_bucket_name`에
+이름을 채워주세요.
+
+배포 이후에 `/auth/`에 접속하면 구글 캘린더 연동을 사용할 수 있습니다.
+
+
+[gcp-api]: https://cloud.google.com/apis/
 [incoming-webhook]: https://slack.com/intl/ko-kr/help/articles/115005265063-Slack%EC%9A%A9-%EC%88%98%EC%8B%A0-%EC%9B%B9%ED%9B%84%ED%81%AC
 
 자파 배포의 필요한 설정과 라이브러리를 설치합니다. `install.sh`에서 `zappa_settings.json`을 생성하니 참고해주세요.
@@ -58,11 +86,25 @@ $ zappa update dev
 
 ## API
 
+### `/auth/`
+
+구글 캘린더 연동을 위해서 사용자의 구글 아이디로 토큰을 발급합니다. OAuth Application 설정 이후 최초 1회 실행하면됩니다.
+
+### `/force-refresh/`
+
+구글 캘린더 연동을 위해서 사용자의 구글 아이디로 토큰을 새로 발급합니다. OAuth Application 설정 이후 뭔가 잘안되면 실행해주세요.
+
 ### `/return/<이름>/`
 
 "이름"님이 자리로 돌아옴을 슬랙 채널에 알립니다. 리소스의 `.["message"]["return"]` 값을 바꾸어 커스텀 메시지로 바꿀 수 있습니다.
 
+### `/calendar/`
+
+"이름"님이 근무를 시작함을 구글 캘린더에 연동하고 슬랙 채널에 알립니다. 리소스의 `.["message"]["start"]` 값을 바꾸어 커스텀 메시지로 바꿀 수 있습니다.
+
 ### `/start/<이름>/`
+
+- `/calendar/`를 사용하면 구글 캘린더와 연동할 수 있습니다. 이 API는 슬랙 알림만 전송됩니다.
 
 "이름"님이 근무를 시작함을 슬랙 채널에 알립니다. 리소스의 `.["message"]["start"]` 값을 바꾸어 커스텀 메시지로 바꿀 수 있습니다.
 
--- web.py
+++ web.py
@@ -27,6 +27,36 @@ CORS(app)
 app.secret_key = base64.b64encode(os.urandom(64)).decode()
 slack_hook = os.environ.get('slack_hook', config.get('slack_hook'))
 kst = datetime.timezone(datetime.timedelta(hours=9))
+config_google = config.get('google', {})
+SCOPES = config_google.get('scopes')
+google_config = {
+    'web': {
+        'client_id': os.environ.get(
+            'google_client_id', config_google.get('client_id')
+        ),
+        'client_secret':os.environ.get(
+            'google_client_secret', config_google.get('client_secret')
+        ),
+        'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+        'token_uri': 'https://accounts.google.com/o/oauth2/token',
+        'project_id': os.environ.get(
+            'google_project_id', config_google.get('project_id')
+        ),
+    }
+}
+bucket_name = config.get('token_bucket_name', 'zappa-gather-webhook-token')
+token_jsonname = 'token_json'
+
+
+def creds_to_dict(credentials):
+    return {
+        'token': credentials.token,
+        'refresh_token': credentials.refresh_token,
+        'token_uri': credentials.token_uri,
+        'client_id': credentials.client_id,
+        'client_secret': credentials.client_secret,
+        'scopes': credentials.scopes,
+    }
 
 
 @app.route('/')
@@ -96,6 +126,163 @@ def make_action():
     dt = datetime.datetime.now(kst) + \
             datetime.timedelta(seconds=int(time) * 60)
     send_text(config['message']['action'].format(name, action, dt))
+    return 'done'
+
+
+def write(bucket, payload):
+    with io.BytesIO() as stream:
+        stream.write(json.dumps(payload).encode())
+        stream.seek(0)
+        bucket.upload_fileobj(stream, token_jsonname)
+
+
+def read(bucket):
+    with io.BytesIO() as stream:
+        try:
+            bucket.download_fileobj(token_jsonname, stream)
+            stream.seek(0)
+            return json.loads(stream.read())
+        except:
+            return None
+
+
+@app.route('/auth/')
+def auth_token():
+    s3 = resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    payload = {}
+    creds_json = read(bucket)
+    creds = None
+    redirect_uri = url_for('.oauth', _external=True)
+    if creds_json:
+        creds = Credentials(
+            token=creds_json['token'],
+            refresh_token=creds_json['refresh_token'],
+            token_uri=creds_json['token_uri'],
+            client_id=creds_json['client_id'],
+            client_secret=creds_json['client_secret'],
+            scopes=creds_json['scopes']
+        )
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+            write(bucket, creds_to_dict(creds))
+            return 'cred refresh'
+        else:
+            flow = Flow.from_client_config(google_config, SCOPES)
+            flow.redirect_uri = redirect_uri
+            authorization_url, state = flow.authorization_url(
+                access_type='offline',
+                include_granted_scopes='true'
+            )
+            session['state'] = state
+            return redirect(authorization_url)
+    return 'already valid'
+
+
+@app.route('/oauth/')
+def oauth():
+    state = session['state']
+    redirect_uri = url_for('.oauth', _external=True)
+    flow = Flow.from_client_config(google_config, SCOPES, state=state)
+    flow.redirect_uri = redirect_uri
+    authorization_response = request.url
+    flow.fetch_token(authorization_response=authorization_response)
+    credentials = flow.credentials
+    s3 = resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    write(bucket, creds_to_dict(credentials))
+    return '200'
+
+
+@app.route('/force-refresh/')
+def force_refresh():
+    s3 = resource('s3')
+    try:
+        bucket = s3.Bucket(bucket_name)
+        bucket.delete_objects(
+            Delete={
+                'Objects': [{'Key': token_jsonname}],
+                'Quiet': True,
+            },
+        )
+    finally:
+        return redirect(url_for('.auth_token'))
+
+
+def get_start_dt():
+    if request.form.get('start_modified') == 'y':
+        start_year = request.form.get('start_year', type=int)
+        start_month = request.form.get('start_month', type=int)
+        start_day = request.form.get('start_day', type=int)
+        start_hour = request.form.get('start_hour', type=int)
+        start_min = request.form.get('start_min', type=int) or 0
+        dt = datetime.datetime(
+            start_year,
+            start_month,
+            start_day,
+            start_hour,
+            start_min,
+            tzinfo=kst
+        )
+    else:
+        dt = datetime.datetime.now(kst)
+    return dt
+
+
+@app.route('/calendar/', methods=['GET'])
+def calendar():
+    return render_template('calendar.html', team_mates=config['teamMates'])
+
+
+@app.route('/calendar-sync/', methods=['POST'])
+def calendar_sync():
+    s3 = resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    creds_json = read(bucket)
+    creds = None
+    if creds_json:
+        creds = Credentials(
+            token=creds_json['token'],
+            refresh_token=creds_json['refresh_token'],
+            token_uri=creds_json['token_uri'],
+            client_id=creds_json['client_id'],
+            client_secret=creds_json['client_secret'],
+            scopes=creds_json['scopes']
+        )
+    if not creds:
+        raise ValueError('creds failed')
+    elif creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+        write(bucket, creds_to_dict(creds))
+    name = request.form.get('name')
+    service = build('calendar', 'v3', credentials=creds)
+    start_dt = get_start_dt()
+    hour = request.form.get('hour', type=int) * 60 * 60
+    minutes = request.form.get('min', type=int) * 60
+    summary =  request.form.get('name')
+    calendar_id = config_google.get('work_calendar_id')
+    response = service.events().insert(
+        calendarId=calendar_id,
+        body={
+            'creator': {
+                'self': True,
+            },
+            'summary': summary,
+            'start': {
+                'timezone': 'Asia/Seoul',
+                'dateTime': start_dt.isoformat(),
+            },
+            'end': {
+                'timezone': 'Asia/Seoul',
+                'dateTime':  (
+                    start_dt + datetime.timedelta(seconds=hour + minutes)
+                ).isoformat(),
+            },
+        },
+    ).execute()
+    text = config['message']['start']
+    send_text(f'{name}: {text}')
     return 'done'
 
 
